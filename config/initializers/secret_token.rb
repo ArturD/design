cb89820f337a758a1872ab8fde8f34cd95cecd1e
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-12-11 23:59:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lernhub::Application.config.secret_token = '98f994f4f50a8f41f9bc6f9e641c75be02f8797f60aa9e543e5670495f25560fc81574f39547e6f6a7fd67fef417cae7aa1b012b9c0958f8b23a35021e14fc56'
